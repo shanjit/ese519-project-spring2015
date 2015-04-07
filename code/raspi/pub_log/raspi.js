4b53a8557faaf4cb7e3c64705d42061e4c63d0db
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2015-04-07 01:41:04 -0400

    pub_log raspi working and checked

--- code/raspi/pub_log/raspi.js
+++ code/raspi/pub_log/raspi.js
@@ -1,32 +1,20 @@
+// Assumptions
+// 1. node.js is installed and so is socket.io
+// 2. socket.io is located at the path below
+
 var io = require('/usr/lib/node_modules/socket.io').listen(8080);
-// Be sure to change path to whereever socket.io is installed on the system
 
-
-var t;  // I usually don't like using global variables but hope it's ok for DEMO's purpose
- 
 function rnd() {
     var num=Math.floor(Math.random()*1000);
     return num;
 }
+
 io.sockets.on('connection', function (socket) {
-    t=setInterval( function() {
+    
+    // At an interval of 1000 ms keep pushing out random numbers
+    setInterval( function() {
         var n=rnd();
         socket.broadcast.emit('stream', {n:n.toString()});
     }, 1000);
-    socket.on('action', function (data) {
-        console.log('received action');
-        if(data.todo=='stop') {
-            socket.broadcast.emit('stream', {n:'Stopped'});
-            console.log('stopping timer now.');
-            clearInterval(t);
-        } else if(data.todo='run') {
-            // the setInterval code definitely can
-            // be combined/optimized with the one above
-            // again for DEMO's sake I just leave it as is
-            t=setInterval( function() {
-                var n=rnd();
-                socket.broadcast.emit('stream', {n:n.toString()});
-            }, 1000);
-        }
-    });
+
 });
