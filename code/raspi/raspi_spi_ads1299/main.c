474307aaf3d00ffc2005ed6bf63f45da3b9f2ea7
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2015-04-10 19:09:05 -0400

    latest pi code

--- code/raspi/raspi_spi_ads1299/main.c
+++ code/raspi/raspi_spi_ads1299/main.c
@@ -14,28 +14,75 @@
 #include <stdint.h>
 
 
+
+
 int main(int argc, char **argv)
 {
 
+	// Initialize the library 
+	// Pin Numbers 
+	// MOSI - P19 on J8 on Pi B+ 
+	// MISO - P21 on J8 on Pi B+
+	// SS - P24 on J8 on Pi B+
+	// SCLK - P23 on J8 on Pi B+
+	// GND - P25 on J8 on Pi B+
+	// Note : The CS is automatically managed by the library
+
+	// SPI Mode configuration and set DRDY pin to be input
 	int val = initLibrary();
-	
-	printf("%d", val);
-
+	//printf("%d", val);
 	if(val!=0)
-	return 23;
-
-	while(1)
 	{
-	uint8_t send_data = 0xaa;
-    uint8_t read_data = transferData(send_data);
-
-    printf("%02x\n", read_data);
-
+		printf("Initialization problem \n Exiting \n");
+		return 23;
 	}
+
+		
+
+	
+	// reset ads1299
+	reset(); 
+
+	uint8_t DeviceId = getDeviceId();
+	printf("Device Id is %02x \n", DeviceId);
+
+	/*
+	if (DeviceId==0xff)
+	{
+		printf("Incorrect configuration.\n");
+	}
+
+	else
+	{
+		printf("Correct configuration. \n");
+	}
+	*/
+	
+
 
 	transferComplete();
 
-
-
 	return 0;
 }
+
+
+/*
+
+	blinky code - use for debug later
+
+    // Set the pin to be an output
+    bcm2835_gpio_fsel(PIN_DRDY, BCM2835_GPIO_FSEL_OUTP);
+
+	// Turn it on
+    bcm2835_gpio_write(PIN, HIGH);
+        
+    // wait a bit
+    bcm2835_delay(500);
+        
+    // turn it off
+    bcm2835_gpio_write(PIN, LOW);
+        
+    // wait a bit
+    bcm2835_delay(500);
+	
+*/
