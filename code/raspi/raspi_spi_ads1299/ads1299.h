e4850fea005820cfc23b26b33ac068a6b4a5f0e5
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2015-04-10 19:09:42 -0400

    Update ads1299.h

--- code/raspi/raspi_spi_ads1299/ads1299.h
+++ code/raspi/raspi_spi_ads1299/ads1299.h
@@ -1,3 +1,4 @@
+// #ifdef 
 
 #include <stdint.h>
 
@@ -5,5 +6,12 @@ int initLibrary();
 uint8_t transferData(uint8_t);
 void transferComplete();
 
+
+void reset();
+uint8_t getDeviceId();
+
 // Implement later
 // uint8_t transfernData(uint8_t);
+
+
+// #endif
