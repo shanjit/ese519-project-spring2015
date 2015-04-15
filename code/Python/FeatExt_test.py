78a91aed2d02782484c698498a8c96024eec5b55
jaredwil <jaredwil@seas.upenn.edu>
2015-04-14 22:21:24 -0400

    Python FeatExt Update

--- code/Python/FeatExt_test.py
+++ code/Python/FeatExt_test.py
@@ -6,6 +6,7 @@ is identical to the values found in the MATLAB model
 import numpy as np
 from pylab import *
 
+
 ############################################
 # Define all constants here
 
@@ -29,10 +30,11 @@ winDisp = 1; #Seconds
 ch = array([27, 3, 6, 2, 24, 7, 11, 20]); #channels of interest 4-Front 2-Temp 2-Occ
 ##########################################
 # Functions
-
+#used to comput the num of windows
 def NumWins(xLen,fs,winLen,winDisp): 
     return round((xLen-(winLen - winDisp)*fs)/(winDisp*fs));
-    
+
+#used to compute line Length    
 def LLFn(x): 
     return sum(abs(np.diff(x)));
 ###########################################
@@ -40,7 +42,7 @@ def LLFn(x):
 
 #csv
 labels = np.loadtxt('C:\Users\Jared\Dropbox\DEAPdatasets\Preprocessed_csv\s01Labels.csv',delimiter=',')
-data = np.loadtxt('C:\Users\Jared\Dropbox\DEAPdatasets\Preprocessed_csv\s01Datav2.csv',delimiter=',',usecols=range(1,8064),skiprows=0)
+data = np.loadtxt('C:\Users\Jared\Dropbox\DEAPdatasets\Preprocessed_csv\s01Datav2.csv',delimiter=',',usecols=range(1,8064*20),skiprows=0)
 
 
 #Spectrogram dependent Information
@@ -68,44 +70,61 @@ show()
 """
 #######################################################################
 #Feature extraction starts here
+totFeats = 82; #total number of features to be used
 
+#initialize arrays for faster computing
+asymFeats = np.empty([windows, (int(freqBands.shape[0])*3)],dtype = float)
+F  = np.empty([windows, totFeats],dtype = float);
 
 for i in range(0,(ch.size)):
+    
+    Pxx, freqs, bins, im = specgram(data[ch[i],:], NFFT=NFFT, Fs=Fs, noverlap=(winLen-winDisp)*Fs, cmap=None);
+    #init freq features vector to be size of ch x number of windows   
+    freqFeats = np.empty([int(freqBands.shape[0]), int(Pxx.shape[1])],dtype = float)
+        
+    for j in range(0,int(freqBands.shape[0])):
+        bandInds = np.logical_and(freqs >= freqBands[j,0] , freqs <= freqBands[j,1]);
+        freqFeats[j,:] = np.mean(np.abs(Pxx[bandInds,:]),0);
+        #end loop
+       
+    timeavg_bin = np.empty([windows,1]);    
+    C = np.convolve(data[ch[i],:],np.ones(winLen*Fs)/(winLen*Fs),'valid');
+    timeavg_bin[:,0] = C[1:C.size:(winDisp)*Fs];
+   
+   
+    #Time Windowed Features
+    xWin = np.empty([windows,winLen*Fs],dtype = float);
+    LL = np.empty([windows,1]);
+    for w in range(0,int(windows)):
+        xWin[w,:] = data[ch[i],(w)*dispSamp:(w)*dispSamp + winLen*Fs];    
+        LL[w] = LLFn(xWin[w,:]);
+    
 
-    Pxx, freqs, bins, im = specgram(data[ch[i],:], NFFT=NFFT, Fs=Fs,
-                                    noverlap=(winLen-winDisp)*Fs, cmap=None);
             #below this line is matlab code
 #########################################################
-                                
-   freqFeats = array([[],[]]);
-   for j in range(0,windows):
-
-       bandInds = np.logical_and(freqs >= freqBands[j,0] , freqs <= freqBands[j,1]);
-       freqFeats[j,:] = np.mean(abs(Pxx[bandInds,:]),1);
-        
-   C = conv(x(i,:),ones(1,winLen*fs)/(winLen*fs),'valid');
-   timeavg_bin = C(1:(winDisp)*fs:end)';
-
-   LL = MovingWinFeats(x(i,:), fs, winLen, winDisp, LLFn);
-    
    #F,T,O (Frontal, Temporal, and Occipital assymetry index)
    #kept addition row of zeros so could be formated into grid
-   if i == 2  
-       asymFeats(:,1:6) = (freqFeats' - F(:,end-6:end-1))./(freqFeats' + F(:,end-6:end-1));
-   else if i == 6
-       asymFeats(:,9:14) = (freqFeats' - F(:,end-6:end-1))./(freqFeats' + F(:,end-6:end-1));
-   else if i == 8
-        asymFeats(:,17:22) = (freqFeats' - F(:,end-6:end-1))./(freqFeats' + F(:,end-6:end-1));
+    if i == 1:  
+        asymFeats[:,0:6] = (np.transpose(freqFeats) - F[:,0:6])/(np.transpose(freqFeats) + F[:,0:6]);
+    elif i == 5:
+        asymFeats[:,6:12] =  (np.transpose(freqFeats) - F[:,32:38])/(np.transpose(freqFeats) + F[:,32:38]);
+    elif i == 7:
+        asymFeats[:,12:18] =  (np.transpose(freqFeats) - F[:,48:54])/(np.transpose(freqFeats) + F[:,48:54]);
         
     
     #make feature matrix
-    F = [F freqFeats' timeavg_bin LL];
+    tmp = np.append(np.transpose(freqFeats), LL,axis = 1)
+    F[:,(i*ch.size):(i*ch.size)+8] = np.append(tmp, (timeavg_bin),axis = 1)
 
-    fprintf('%d',i)   
-    end
+    print(i)   
+    #end loop
     
-    F = [F asymFeats];
-    
+F[:,-18:] = asymFeats;
 
+featAv = np.mean(F,0);
+featStd = np.std(F,0); 
+    
+for i in range(0,totFeats):
+    F[:,i] = (F[:,i] - featAv[i])/featStd[i]
 
 
